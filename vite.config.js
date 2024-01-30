99f9b52c2634d5b409e3d4f29f357d30ffd9a55d
pradeep <pradeep>
2024-01-30 17:57:42 +0530

    proxy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api/v1': 'https://video-backend-3ot2.onrender.com'
+      '/api': 'https://video-backend-3ot2.onrender.com'
     }
   },
   plugins: [react()],
