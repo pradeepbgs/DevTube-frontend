b49335e5be65c9e93e6cc0947aa71c280978ebca
pradeep <pradeep>
2024-02-01 11:46:56 +0530

    proxy 4

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     
     try {
       const response = await axios.post(
-        "/api/v1/users/login",
+        "https://video-backend-3ot2.onrender.com/api/v1/users/login",
         {
           email,
           password,
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://video-backend-3ot2.onrender.com'
-    }
-  },
   plugins: [react()],
+  // server: {
+  //   proxy: {
+  //     '/api': "https://video-backend-3ot2.onrender.com"
+  //   }
+  // }
 })
