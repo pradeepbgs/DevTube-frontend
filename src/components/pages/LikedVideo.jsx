3cb5992d6f4e05650c4f65da314ab4c60edda57c
pradeep <kumarpradeepbgs@gmail.com>
2024-02-17 14:00:37 +0530

    just made  liked video

--- src/components/pages/LikedVideo.jsx
+++ src/components/pages/LikedVideo.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import VideoCard from '../videoComponents/VideoCard';
+import axios from 'axios';
 
 const LikedVideo = () => {
-  return (
-    <div className='text-white'>
-      this is liked video page
-    </div>
-  )
-}
+  const [likedVideos, setLikedVideos] = useState([]);
+  
+  const fetchLikedVideos = async () => {
+    try {
+      const response = await axios.get(`/api/v1/likes/videos`, { withCredentials: true });
+      setLikedVideos(response?.data?.data);
+    } catch (error) {
+      console.error('Error fetching liked videos:', error);
+    }
+  };
 
-export default LikedVideo
+  useEffect(() => {
+    fetchLikedVideos();
+  }, []);
+
+  console.log(likedVideos);
+
+  return (
+    <div className='text-white flex flex-wrap'>
+      {likedVideos?.map((video,index) => (
+        <div key={index} className='flex'>
+        <VideoCard video={video} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LikedVideo;
